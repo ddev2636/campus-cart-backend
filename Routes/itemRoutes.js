3373a1f06ad68dcc1a9a1a1fee09b5f9f31c1098
Debaditya <ddev2636@gmail.com>
2023-07-26 22:57:37 +0530

    testing

--- Routes/itemRoutes.js
+++ Routes/itemRoutes.js
@@ -5,7 +5,7 @@ import { sellThing, buyThing } from "../controllers/itemControllers.js";
 //import upload from "../middleware/upload.js";
 
 // router.route("/sell").post(upload.single("image"), sellThing);
-router.route("/sell", sellThing);
+router.post("/sell", sellThing);
 //router.post("/sell", upload.single("image"), sellThing);
 router.get("/", buyThing);
 export default router;
